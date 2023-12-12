920dac02a83d17f0ecd730d5f49935c332e5f85a
Guzun Cezar <Guzun Cezar>
2023-12-13 00:23:00 +0200

    Initial commit

---
+++ Lab_2/Base_laboratory.py
@@ -0,0 +1,142 @@
+import os
+from datetime import datetime
+
+class File:
+    def __init__(self, filename, extension, created_time, updated_time):
+        self.filename = filename
+        self.extension = extension
+        self.created_time = created_time
+        self.updated_time = updated_time
+
+    def get_info(self):
+        return f"File: {self.filename}, Extension: {self.extension}, Created: {datetime.fromtimestamp(self.created_time)}, Updated: {datetime.fromtimestamp(self.updated_time)}"
+
+class ImageFile(File):
+    def __init__(self, filename, extension, created_time, updated_time, image_size):
+        super().__init__(filename, extension, created_time, updated_time)
+        self.image_size = image_size
+
+    def get_info(self):
+        return super().get_info() + f", Image Size: {self.image_size}"
+
+class TextFile(File):
+    def __init__(self, filename, extension, created_time, updated_time, line_count, word_count, char_count):
+        super().__init__(filename, extension, created_time, updated_time)
+        self.line_count = line_count
+        self.word_count = word_count
+        self.char_count = char_count
+
+    def get_info(self):
+        return super().get_info() + f", Lines: {self.line_count}, Words: {self.word_count}, Characters: {self.char_count}"
+
+class ProgramFile(File):
+    def __init__(self, filename, extension, created_time, updated_time, line_count, class_count, method_count):
+        super().__init__(filename, extension, created_time, updated_time)
+        self.line_count = line_count
+        self.class_count = class_count
+        self.method_count = method_count
+
+    def get_info(self):
+        return super().get_info() + f", Lines: {self.line_count}, Classes: {self.class_count}, Methods: {self.method_count}"
+
+class FolderMonitor:
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+        self.files = {}
+        self.snapshot_time = 0
+        self.previous_files = set()
+
+    def commit(self):
+        self.scan_folder()
+        self.previous_files = set(self.files.keys())
+        self.snapshot_time = datetime.now()
+        print(f"Created Snapshot at: {self.snapshot_time}")
+
+
+    def scan_folder(self):
+        self.files = {}
+        for filename in os.listdir(self.folder_path):
+            file_path = os.path.join(self.folder_path, filename)
+            if os.path.isfile(file_path):
+                file_obj = self.get_file_info(file_path)
+                self.files[file_obj.filename] = file_obj
+    def get_file_info(self, file_path):
+        filename, file_extension = os.path.splitext(os.path.basename(file_path))
+        created_time = os.path.getctime(file_path)
+        updated_time = os.path.getmtime(file_path)
+
+        if file_extension.lower() in ['.png', '.jpg']:
+            image_size = self.get_image_size(file_path)
+            return ImageFile(filename, file_extension, created_time, updated_time, image_size)
+
+        elif file_extension.lower() == '.txt':
+            line_count, word_count, char_count = self.get_text_file_stats(file_path)
+            return TextFile(filename, file_extension, created_time, updated_time, line_count, word_count, char_count)
+
+        elif file_extension.lower() in ['.py', '.java']:
+            line_count, class_count, method_count = self.get_program_file_stats(file_path)
+            return ProgramFile(filename, file_extension, created_time, updated_time, line_count, class_count,
+                               method_count)
+
+        else:
+            return File(filename, file_extension, created_time, updated_time)
+
+    def get_image_size(self, file_path):
+        return "1024x860"
+
+    def get_text_file_stats(self, file_path):
+        with open(file_path, 'r') as file:
+            content = file.read()
+            lines = content.split('\n')
+            words = content.split()
+            return len(lines), len(words), len(content)
+
+    def get_program_file_stats(self, file_path):
+        return 100, 5, 20
+
+    def info(self, filename):
+        if filename.lower() == "all files":
+            for file_obj in self.files.values():
+                print(file_obj.get_info())
+        elif filename in self.files:
+            print(self.files[filename].get_info())
+        else:
+            print(f"File '{filename}' not found in the monitored folder.")
+
+    def status(self):
+        self.scan_folder()
+        print(f"Created Snapshot at: {self.snapshot_time}")
+
+        for filename, file_obj in self.files.items():
+            if filename in self.previous_files:
+                changed = file_obj.updated_time > self.snapshot_time.timestamp()
+                print(f"{filename} - {'No change' if not changed else 'Changed'}")
+            else:
+                print(f"{filename} - New File")
+
+        for filename in set(self.previous_files) - set(self.files.keys()):
+            print(f"{filename} - Deleted")
+
+
+if __name__ == "__main__":
+    monitored_folder =r"C:\Users\guzun\PycharmProjects\OOP_laboratory\lab_1"
+    folder_monitor = FolderMonitor(monitored_folder)
+
+    while True:
+        action = input("Enter action (commit, info <filename>, status): ").split()
+
+        if action[0] == "commit":
+            folder_monitor.commit()
+
+        elif action[0] == "info":
+            if len(action) == 2:
+                folder_monitor.info(action[1])
+            else:
+                print("Invalid 'info' command. Please provide a filename.")
+
+        elif action[0] == "status":
+            folder_monitor.scan_folder()
+            folder_monitor.status()
+
+        else:
+            print("Invalid command. Please enter a valid action.")
---
+++ lab_1/Base_Laboratory.py
@@ -0,0 +1,241 @@
+from datetime import datetime
+import  pickle
+
+DOB_FORMAT = "%d-%m-%Y"
+
+
+class Student:
+    def __init__(self, first_name, last_name, email, date_of_birth):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.enrollment_date = datetime.now().date()
+        self.date_of_birth = datetime.strptime(date_of_birth, DOB_FORMAT)
+        self.faculty = None
+
+    def __str__(self):
+        return f"Name: {self.first_name} {self.last_name}, Enrolled: {self.enrollment_date}, Birthday: {self.date_of_birth}"
+
+
+class Faculty:
+    def __init__(self, name, abbreviation, study_field):
+        self.name = name
+        self.abbreviation = abbreviation
+        self.study_field = study_field
+        self.students = []
+        self.graduates = []
+
+    def create_student(self, student):
+        self.students.append(student)
+        student.faculty = self
+        Logger.log_operation(f"Created student: {student.first_name} {student.last_name} in {self.abbreviation}")
+
+    def graduate_student(self, student):
+        if student in self.students:
+            self.students.remove(student)
+            self.graduates.append(student)
+            Logger.log_operation(f"Graduated student: {student.first_name} {student.last_name} from {self.abbreviation}")
+            return f"{student} has graduated"
+        else:
+            Logger.log_operation(f"Failed to graduate. {student} is not enrolled in {self.abbreviation}")
+            return f"{student} is not enrolled in this faculty"
+
+    def enrolled_students(self):
+        print(f"Currently enrolled at {self.abbreviation}")
+        for student in self.students:
+            print(f"{student.first_name} {student.last_name}")
+
+    def graduated(self):
+        print(f"Graduated students from {self.abbreviation}")
+        for student in self.graduates:
+            print(f"{student.first_name} {student.last_name}")
+
+    def is_enrolled(self, student):
+        return student in self.students
+
+    def student_by_email(self, email):
+        for student in self.students:
+            if student.email == email:
+                return student
+        return student
+
+
+
+    def __repr__(self):
+        return f"{self.abbreviation} {self.name}"
+
+
+class University:
+    def __init__(self):
+        self.faculties = []
+
+    def create_faculty(self, faculty):
+        self.faculties.append(faculty)
+
+    def faculty_by_student_email(self, email):
+        for faculty in self.faculties:
+            for student in faculty.students:
+                if student.email == email:
+                    return faculty
+        return faculty
+
+
+    def all_faculties(self):
+        return self.faculties
+
+    def faculty_by_abbreviation(self, abbreviation):
+        for faculty in self.faculties:
+            if faculty.abbreviation == abbreviation:
+                return faculty
+        return None
+
+    def faculties_by_study_field(self, study_field):
+        return [faculty for faculty in self.faculties if faculty.study_field == study_field]
+
+
+class SaveManager:
+    SAVE_FILE = 'student_management_state.pkl'
+
+    @staticmethod
+    def save_state(university):
+        with open(SaveManager.SAVE_FILE, 'wb') as file:
+            pickle.dump(university, file)
+
+    @staticmethod
+    def load_state():
+        try:
+            with open(SaveManager.SAVE_FILE, 'rb') as file:
+                return pickle.load(file)
+        except FileNotFoundError:
+            return University()
+class Logger:
+    LOG_FILE = 'operation_logs.txt'
+
+    @staticmethod
+    def log_operation(operation_description):
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        log_entry = f"{timestamp} - {operation_description}\n"
+        with open(Logger.LOG_FILE, 'a') as log_file:
+            log_file.write(log_entry)
+
+
+
+
+
+if __name__ == '__main__':
+    UTM = SaveManager.load_state()
+
+    while True:
+        print("Welcome to TUM's student management system!")
+        print("\nWhat do you want to do?")
+        print('g - General operations\nf - Faculty operations \ns - Student operations \nq - Quit program')
+
+        choice = input("Your input: ")
+
+        # General operations
+        if choice.lower() == 'g':
+            print("\nGeneral operations \nWhat do you want to do?")
+            choice = input("nf/<faculty name>/<faculty abbreviation>/<field of study> - create faculty"
+                           "\nss/<student email> - search student and show faculty"
+                           "\ndf - display faculties"
+                           "\ndf/<field> - display all faculties of a field"
+                           "\n\n\nb - Back"
+                           "\nq - Quit program"
+                           "\n\n\nYour input: ")
+            if choice[0:2] == 'nf':
+                data = choice[3:].split("/")
+                name, abbreviation, study_field = data
+                faculty = Faculty(name, abbreviation, study_field)
+                UTM.create_faculty(faculty)
+                Logger.log_operation(f"Created faculty: {faculty.name} ({faculty.abbreviation})")
+            elif choice[0:2] == 'ss':
+                data = choice[3:]
+                faculty = UTM.faculty_by_student_email(data)
+                if faculty:
+                    print(f"Student with email {data} is enrolled in {faculty}")
+                else:
+                    print(f"No such student with email {data}")
+            elif choice[0:2] == 'df':
+                print(UTM.all_faculties())
+            elif choice[0:3] == 'df/':
+                data = choice[3:]
+                faculties = UTM.faculties_by_study_field(data)
+                print(f"Faculties in the field {data}: {faculties}")
+
+        # Faculty operations
+        elif choice.lower() == 'f':
+            print("\nFaculty operations \nWhat do you want to do?")
+            choice = input(
+                "\nns/<faculty abbreviation>/<first name>/<last name>/<email>/<day>/<month>/<year> - create student"
+                "\ngs/<email> - graduate student"
+                "\nds/<faculty abbreviation> - display enrolled students"
+                "\ndg/<faculty abbreviation> - display graduated students"
+                "\nbf/<faculty abbreviation>/<email> - check if student belongs to faculty"
+                "\n\n\nb - Back"
+                "\nq - Quit program"
+                "\n\n\nYour input: ")
+            if choice[0:2] == 'ns':
+                data = choice[3:].split("/")
+                abrev, fn, ln, email, d, m, y = data
+                birthday = f'{d}-{m}-{y}'
+                new_student = Student(fn, ln, email, birthday)
+                faculty = UTM.faculty_by_abbreviation(abrev)
+                if faculty:
+                    faculty.create_student(new_student)
+                else:
+                    print(f"No such faculty with abbreviation {abrev}")
+                Logger.log_operation(f"Created student: {new_student.first_name} {new_student.last_name} in {faculty.abbreviation}")
+            elif choice[0:2] == 'gs':
+                data = choice[3:].split("/")
+                faculty = UTM.faculty_by_student_email(data)
+                student = faculty.student_by_email(data)
+                if student:
+                    print(f"{faculty.graduate_student(student)}")
+                else:
+                    print(f"No such student with email {data}")
+                Logger.log_operation(f"Failed to graduate student: {data}. Student not present or not enrolled.")
+            elif choice[0:2] == 'ds':
+                data = choice[3:]
+                faculty = UTM.faculty_by_abbreviation(data)
+                if faculty:
+                    faculty.enrolled_students()
+                else:
+                    print(f"No such faculty with abbreviation {data}")
+                Logger.log_operation(f"Displayed enrolled students for {faculty.abbreviation}")
+            elif choice[0:2] == 'dg':
+                data = choice[3:]
+                faculty = UTM.faculty_by_abbreviation(data)
+                if faculty:
+                    faculty.graduated()
+                else:
+                    print(f"No such faculty with abbreviation {data}")
+            elif choice[0:2] == 'bf':
+                data = choice[3:].split("/")
+                abrev, email = data
+                faculty = UTM.faculty_by_abbreviation(abrev)
+                if faculty:
+                    student = faculty.student_by_email(email)
+                    if student:
+                        print(f"{student} is enrolled in {faculty}")
+                    else:
+                        print(f"No such student with email {email}")
+                else:
+                    print(f"No such faculty with abbreviation {abrev}")
+
+
+        elif choice.lower() == "s":
+            print("\nStudent operations \nWhat do you want to do?")
+            choice = input("\nns/<first_name>/<last_name>/<email>/<date_of_birth> - create student"
+                           "\n\n\nb - Back"
+                           "\nq - Quit program"
+                           "\n\n\nYour input: ")
+            if choice[0:2] == 'ns':
+                data = choice[3:].split("/")
+                fn, ln, email, dob = data
+                new_student = Student(fn, ln, email, dob)
+
+        # Quit program
+        elif choice.lower() == 'q':
+            SaveManager.save_state(UTM)
+            Logger.log_operation("Quit program")
+            break
